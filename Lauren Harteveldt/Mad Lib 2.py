4b4a6ca8ee20df09dd9837bc60997c11ea67db74
lhasapis <lhasapis@gcds.net>
2017-11-15 14:36:02 -0500

    Changed Formatting and added time
    I fixed a few print statements and added import time.

--- Lauren Harteveldt/Mad Lib 2.py
+++ Lauren Harteveldt/Mad Lib 2.py
@@ -1,22 +1,39 @@
 import time
 
-print ("Write a person!")
-person1 = input ()
+###Mad Lib###
 
 print ("Write a person!")
 person1 = input ()
 
-"Do you want to make " + person1 "'s " + " famous " + food1 + "recipe?"
+print ("Write a food!")
+food1 = input()
+
+print("Write an adjective!")
+adjective1 = input()
+
+print("Write a noun!")
+noun1 = input()
+
+print("Write a number above 300!")
+number1 = input()
+
+print("Write a verb ending in ing")
+verbing = input()
+
+
+###Begin Madlib###
+
+print("Do you want to make " + person1 "'s " + " famous " + food1 + "recipe?"
 "It is absolutly " + adjective1 "."
-"First you have to preheat the " + noun "to " + amount of degrees in F + "."
-"Once you do that you can start " + verb ending in ing + person1 "'s " + "famous " + food1
-+ "recipie!"
+"First you have to preheat the " + noun1 "to " + number1 + "."
+"Once you do that you can start " + verbing + person1 "'s " + "famous " + food1
++ "recipie!")
 
 
-time.sleep(30)
 
 
-Preheat oven to 375f or 190c; line muffin cups with papers.
-Cream butter and sugar till light and fluffy. beat in eggs one at a time.
-Add flour (mixed with baking powder and salt) alternating with milk beat well; stir in vanilla.
-Divide evenly among pans and bake for 18 minutes. let cool in pans.
+print ("Preheat oven to 375f or 190c; line muffin cups with papers. Cream butter and sugar till light and fluffy. beat in eggs one at a time. Add flour mixed with baking powder and salt) alternating with milk beat well; stir in vanilla.
+ + "Divide evenly among pans and bake for 18 minutes. let cool in pans.")
+
+
+time.sleep (100)
